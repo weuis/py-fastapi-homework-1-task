e0bc574ff7f442b3359390d0ce1a0a4c1b71b7b0
Алексей Розеган <rozeganalexey@gmail.com>
2025-05-06 14:15:26 +0200

    Fixed solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -40,6 +40,7 @@ async def get_movies(
         "total_items": total_items
     }
 
+
 @router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
 async def get_movie(movie_id: int, db: AsyncSession = Depends(get_db)):
     result = await db.execute(select(MovieModel).filter(MovieModel.id == movie_id))
